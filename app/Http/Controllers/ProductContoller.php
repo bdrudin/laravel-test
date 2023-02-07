9b4a0351a45a4d831be3d9ea9ce258cbc3c74a57
bdrudin <badrudin.on@gmail.com>
2023-02-07 08:04:49 +0700

    update routes api

--- app/Http/Controllers/ProductContoller.php
+++ app/Http/Controllers/ProductContoller.php
@@ -7,15 +7,4 @@ use Illuminate\Http\Request;
 class ProductContoller extends Controller
 {
     //
-    public function index()
-    {
-        $response = $this->withCookie('color', 'blue')->get('/');
-
-        $response = $this->withCookies([
-            'color' => 'blue',
-            'name' => 'Taylor',
-        ])->get('/products');
-
-        return response()->json($response);
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +20,10 @@ use App\Http\Controllers\CustomerController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-// get api customers
+// api customers
 Route::get('/customers', [CustomerController::class, 'index']);
+
+// api products
+Route::get('/products', function () {
+    return ProductResource::collection(Product::all());
+});
